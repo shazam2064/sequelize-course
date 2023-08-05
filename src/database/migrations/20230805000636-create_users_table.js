a7903248d085f5e30282bf33806ef8990ee9b826
Gabriel Salomon <55660025+shazam2064@users.noreply.github.com>
2023-08-05 17:19:58 -0400

    configured migrations file for users.

--- src/database/migrations/20230805000636-create_users_table.js
+++ src/database/migrations/20230805000636-create_users_table.js
@@ -2,23 +2,45 @@
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    await queryInterface.createTable('Users', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER,
-      }
-    });
-  },
+    async up(queryInterface, Sequelize) {
+        await queryInterface.createTable('Users', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER,
+            },
+            email: {
+                type: Sequelize.STRING(100),
+                allowNull: false,
+                unique: true,
+            },
+            password: {
+                type: Sequelize.STRING,
+                allowNull: false,
+            },
+            username: {
+                type: Sequelize.STRING(50),
+                unique: true,
+            },
+            firstName: {
+                type: Sequelize.STRING(50),
+            },
+            lastName: {
+                type: Sequelize.STRING(50),
+            },
+            createdAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+            updatedAt: {
+                allowNull: false,
+                type: Sequelize.DATE,
+            },
+        });
+    },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+    async down(queryInterface, Sequelize) {
+        await queryInterface.dropTable('Users');
+    }
 };
