c213ccbedca64e99d686ee6baec7a1284eb9d460
Gabriel Salomon <55660025+shazam2064@users.noreply.github.com>
2023-08-05 17:54:06 -0400

    the refresh token migration was added.

--- src/models/refresh-token.js
+++ src/models/refresh-token.js
@@ -3,12 +3,15 @@ import {Model, DataTypes} from "sequelize";
 export default (sequelize) => {
     class RefreshToken extends Model {
         static associate(models) {
-            RefreshToken.belongsTo(models.User);
+            RefreshToken.belongsTo(models.User, {
+                onUpdate: 'CASCADE',
+                onDelete: 'CASCADE',
+            });
         }
     }
 
     RefreshToken.init(
-        {token: {type: DataTypes.STRING}},
+        {token: {type: DataTypes.TEXT}},
         {sequelize, modelName: 'RefreshToken'}
     );
 
